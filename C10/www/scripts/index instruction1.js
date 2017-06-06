955d3daf0a5066722dcc332b02dccabf37447176
Jackson Ng <jacksonng@outlook.com>
2017-06-06 17:42:00 +0800

    Initial Release

---
+++ C10/www/scripts/index instruction1.js
@@ -0,0 +1,22 @@
+﻿(function () {
+
+    $(document).ready(function () {
+        $("#btnMultiply").bind("click", function () {
+            multiply();
+        });
+    });
+
+function multiply() {
+    var from, result;
+    $("#mybody").find("tr").remove();
+    from = parseInt($("#txtFrom").val());
+
+
+    for (i = 1; i <= 10; i++) {
+        result = from * i;
+        $("#mybody").append("<tr><td>" + from.toString() + "</td><td>" + i.toString() + "</td><td>" + result.toString() + "</td></tr>");
+    }
+}
+
+
+})();
---
+++ C10/www/scripts/index instruction2.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    $(document).ready(function () {
+        $("#btnMultiply").bind("click", function () {
+            multiply();
+        });
+    });
+
+    function multiply() {
+        var from, result;
+        $("#mybody").find("tr").remove();
+        from = parseInt($("#txtFrom").val());
+
+        $("#result").empty();
+
+        var htmlstring;
+
+        for (i = 1; i <= 10; i++) {
+            result = from * i;
+            htmlstring = "<a href='#' class='ui-btn' id='btn_" + i + "'>" + from.toString() + "x" + i.toString() + "</a>";
+
+            $("#result").append(htmlstring);
+
+            $("#btn_" + i).bind("click", {id: result}, function (event) {
+                var data = event.data;
+                alert(data.id);
+            });
+        }
+    }
+
+
+})();
