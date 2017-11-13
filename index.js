883d582be86b7708e09cd4fc95547971d552816c
Jackson Ng <jacksonng@outlook.com>
2017-11-13 16:25:32 +0800

    Lecture Quiz index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+    $(document).ready(function () {
+
+        $("#GoToGoogle").bind("click", function () {
+            //Add your codes here
+            
+            
+            
+            
+        });
+    });
+
+})();
